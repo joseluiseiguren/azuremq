5d9e7835257ebc2480cf89f4ed5514536b77cc54
Jose Luis Eiguren <joseluiseiguren@gmail.com>
2018-08-09 17:00:15 +0200

    first commit

---
+++ FireAndForget-Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace FireAndForget_Client
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://mqjoseph.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=5o/AWvaCUclEUShZk6NKocdDXT97/+5t3EP5arVICwA=";
+        const string QueueName = "queue1";
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                //connect to queue
+                queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    //prepare message to send
+                    var messageBody = "Mensaje Prueba Joseph #" + i.ToString();
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    //send message to queue
+                    queueClient.SendAsync(message).Wait();
+
+                    Console.WriteLine(messageBody);
+
+                    System.Threading.Thread.Sleep(1500);
+                }              
+
+                //close connection
+                queueClient.CloseAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);                
+            }
+
+            Console.ReadKey();
+        }        
+    }
+}
---
+++ FireAndForget-Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace FireAndForget_Server
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://mqjoseph.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=5o/AWvaCUclEUShZk6NKocdDXT97/+5t3EP5arVICwA=";
+        const string QueueName = "queue1";
+        static IQueueClient queueClient;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                //connect to queue
+                queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+                //funcion que va a manejar la llegada de los mensajes
+                var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+                {
+                    // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                    // Set it according to how many messages the application wants to process in parallel.
+                    MaxConcurrentCalls = 1,
+
+                    // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                    // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                    AutoComplete = false
+                };
+
+                // Register the function that will process messages
+                queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+
+                //TODO: should close the conection when the application ends
+                //queueClient.CloseAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queueClient is created in ReceiveMode.PeekLock mode (which is default).
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
+            // If queueClient has already been Closed, you may chose to not call CompleteAsync() or AbandonAsync() etc. calls 
+            // to avoid unnecessary exceptions.
+        }
+
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
