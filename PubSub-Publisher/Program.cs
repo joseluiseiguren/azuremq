9a8cff529c093556ee98af3191ebd3e1073dfd49
Jose Luis Eiguren <joseluiseiguren@gmail.com>
2018-08-10 17:37:23 +0200

    pubsub finish

--- PubSub-Publisher/Program.cs
+++ PubSub-Publisher/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Azure.ServiceBus;
 
 namespace PubSub_Publisher
 {
     class Program
     {
+        const string ServiceBusConnectionString = "Endpoint=sb://topi-test-1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Ctk5KTyaGg36Bk7vEJWqDTC+RGWgd31LrCfnz2YEBXA=";
+        const string TopicName = "topic-1";
+        static ITopicClient topicClient;
+
         static void Main(string[] args)
         {
+            try
+            {
+                //connect to topic
+                topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    //prepare message to send
+                    var messageBody = "Mensaje Prueba Joseph #" + i.ToString();
+                    var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+                    //send message to topic
+                    topicClient.SendAsync(message).Wait();
+
+                    Console.WriteLine(messageBody);
+
+                    Thread.Sleep(3000);
+                }
+
+                //close connection
+                topicClient.CloseAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
         }
     }
 }
--- PubSub-Subscriber-LegacyApp/Program.cs
+++ PubSub-Subscriber-LegacyApp/Program.cs
@@ -1,15 +1,78 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
 
 namespace PubSub_Subscriber_LegacyApp
 {
     class Program
     {
+        const string ServiceBusConnectionString = "Endpoint=sb://topi-test-1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=Ctk5KTyaGg36Bk7vEJWqDTC+RGWgd31LrCfnz2YEBXA=";
+        const string TopicName = "topic-1";
+        const string SubscriptionName = "subscription-legacy-system";
+        static ISubscriptionClient subscriptionClient;
+
         static void Main(string[] args)
         {
+            try
+            {
+                //connect to subscription
+                subscriptionClient = new SubscriptionClient(ServiceBusConnectionString, TopicName, SubscriptionName);
+
+                //funcion que va a manejar la llegada de los mensajes
+                var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+                {
+                    // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                    // Set it according to how many messages the application wants to process in parallel.
+                    MaxConcurrentCalls = 1,
+
+                    // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                    // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                    AutoComplete = false
+                };
+
+                // Register the function that will process messages
+                subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+
+                //TODO: should close the conection when the application ends
+                //queueClient.CloseAsync().Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadKey();
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            //simulate processing
+            Thread.Sleep(2000);
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queueClient is created in ReceiveMode.PeekLock mode (which is default).
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
+            // If queueClient has already been Closed, you may chose to not call CompleteAsync() or AbandonAsync() etc. calls 
+            // to avoid unnecessary exceptions.
+        }
+
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
         }
     }
 }
